a404555cd907c0474e55ab235fa895c60ea8ca84
Robert Gallup <bob@xobxob.com>
2013-09-14 07:12:13 -0700

    Fixed target server

--- XOBXOB_Ethernet/XOBXOB_Info.h
+++ XOBXOB_Ethernet/XOBXOB_Info.h
@@ -3,8 +3,8 @@
 #ifndef _xobxob_info_h
 #define _xobxob_info_h
 
-//#define XOBXOB_SERVER_NAME "www.xobxob.com"
-//#define XOBXOB_SERVER_PORT 80
+#define XOBXOB_SERVER_NAME "www.xobxob.com"
+#define XOBXOB_SERVER_PORT 80
 
 static String _LF = "\x0A";
 
