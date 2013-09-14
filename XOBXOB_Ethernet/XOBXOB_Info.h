29636d47b0ba8c9fc0ad40e2fc8a70d249cb4d6d
Robert Gallup <bob@xobxob.com>
2013-09-14 06:49:03 -0700

    Took out reference to local server

--- XOBXOB_Ethernet/XOBXOB_Info.h
+++ XOBXOB_Ethernet/XOBXOB_Info.h
@@ -6,9 +6,6 @@
 //#define XOBXOB_SERVER_NAME "www.xobxob.com"
 //#define XOBXOB_SERVER_PORT 80
 
-#define XOBXOB_SERVER_NAME "192.168.1.107"
-#define XOBXOB_SERVER_PORT 8080
-
 static String _LF = "\x0A";
 
 #endif
